e004d97251682df84a044247ed6e79fd6673a625
degarashi <slice013@gmail.com>
2016-08-16 19:05:10 +0900

    内部形式とメモリ表現を直接比較していた箇所を修正

--- tests/vector_float_test.cpp
+++ tests/vector_float_test.cpp
@@ -28,7 +28,7 @@ namespace frea {
 					ASSERT_NEAR(tsum.m[i], tmul.m[i], threshold);
 			}
 			// 自身との除算は1
-			ASSERT_EQ(v0/v0, vec_t(1));
+			ASSERT_EQ(vec_t(v0/v0), vec_t(1));
 			// 乗算して同じ数で除算すれば大体元と同じになる
 			const vec_t v1 = this->makeRVecNZ(1e-4, range);
 			ASSERT_LT(DiffSum(v0.m, vec_t(v0*v1/v1).m), threshold);
