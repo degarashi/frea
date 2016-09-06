d58617b7cd4243304fa255564d4133d9fe62e29d
degarashi <slice013@gmail.com>
2016-09-07 08:57:35 +0900

    plane: noexcept指定できる所は指定し、コメント付加

--- plane.hpp
+++ plane.hpp
@@ -22,24 +22,25 @@ namespace frea {
 		using mat4_t = typename mat3_t::template type_cn<4,4>;
 
 		PlaneT() = default;
-		constexpr PlaneT(const base_t& b):
-			base_t(b) {}
-		// 違う要素Quatからの変換
+		constexpr PlaneT(const base_t& b) noexcept: base_t(b) {}
+		// 違う内部形式からの変換(float->double など)
 		template <class T2, bool A2>
-		constexpr PlaneT(const PlaneT<T2,A2>& p):
-			base_t(p.asVec4()) {}
-		constexpr PlaneT(const vec_t& orig, const value_t& dist):
+		constexpr PlaneT(const PlaneT<T2,A2>& p) noexcept: base_t(p.asVec4()) {}
+		constexpr PlaneT(const vec_t& orig, const value_t& dist) noexcept:
 			base_t(orig.x, orig.y, orig.z, dist) {}
 
-		constexpr static PlaneT FromPtDir(const vec_t& pos, const vec_t& dir) {
+		//! 座標1つと法線から平面を作成
+		constexpr static PlaneT FromPtDir(const vec_t& pos, const vec_t& dir) noexcept {
 			return {dir, -dir.dot(pos)};
 		}
-		constexpr static PlaneT FromPts(const vec_t& p0, const vec_t& p1, const vec_t& p2) {
+		//! 座標3つから平面を作成
+		constexpr static PlaneT FromPts(const vec_t& p0, const vec_t& p1, const vec_t& p2) noexcept {
 			vec_t nml = (p1 - p0).cross(p2 - p0);
 			nml.normalize();
 			return {nml, -p0.dot(nml)};
 		}
-		static auto ChokePoint(const PlaneT& p0, const PlaneT& p1, const PlaneT& p2) {
+		//! 3つの平面が交差する地点を算出
+		static auto ChokePoint(const PlaneT& p0, const PlaneT& p1, const PlaneT& p2) noexcept {
 			struct {
 				vec_t	pt;
 				bool	cross;
@@ -64,7 +65,8 @@ namespace frea {
 			ret.pt = v;
 			return ret;
 		}
-		static auto CrossLine(const PlaneT& p0, const PlaneT& p1) {
+		//! 2つの平面が交差する直線を算出
+		static auto CrossLine(const PlaneT& p0, const PlaneT& p1) noexcept {
 			struct {
 				vec_t	pt, dir;
 				bool	cross;
@@ -74,7 +76,7 @@ namespace frea {
 			const auto &nml0 = p0.getNormal(),
 						&nml1 = p1.getNormal();
 			vec_t nml = nml0 % nml1;
-			if(std::fabs(nml.len_sq()) < 1e-5)
+			if(std::abs(nml.len_sq()) < 1e-5)
 				return ret;
 			nml.normalize();
 
@@ -98,13 +100,14 @@ namespace frea {
 			return ret;
 		}
 
-		value_t dot(const vec_t& p) const {
+		value_t dot(const vec_t& p) const noexcept {
 			return asVec4().dot(p.template convertI<4,3>(1));
 		}
-		void move(const value_t& d) {
+		//! 平面を法線の方向へ指定距離、移動
+		void move(const value_t& d) noexcept {
 			this->w += d;
 		}
-		const vec_t& getNormal() const {
+		const vec_t& getNormal() const noexcept {
 			return reinterpret_cast<const vec_t&>(*this);
 		}
 		template <class M>
@@ -117,10 +120,12 @@ namespace frea {
 			);
 		}
 		using op_m::operator *;
-		vec_t placeOnPlane(const vec_t& src) const {
+		//! 引数の座標を垂直に平面へ下ろした地点の座標
+		vec_t placeOnPlane(const vec_t& src) const noexcept {
 			const value_t d = dot(src);
 			return src - getNormal() * d;
 		}
+		//! 引数の座標をdir方向に向かって移動させ、平面と交差する座標
 		value_t placeOnPlaneDirDist(const vec_t& dir, const vec_t& src) const {
 			const value_t r = dir.dot(getNormal());
 			if(std::abs(r) < 1e-6) {
@@ -129,23 +134,25 @@ namespace frea {
 			}
 			return -dot(src) / r;
 		}
-		vec_t getOrigin() const {
+		//! 平面上の一点
+		vec_t getOrigin() const noexcept {
 			return getNormal() * this->w;
 		}
+		//! Vector4に読み替え
 		const vec4_t& asVec4() const noexcept {
 			return reinterpret_cast<const vec4_t&>(*this);
 		}
-		bool operator == (const PlaneT& p) const {
+		bool operator == (const PlaneT& p) const noexcept {
 			return asVec4() == p.asVec4();
 		}
 		//! 平面にてベクトルを反転
-		vec_t flip(const vec_t& v) const {
+		vec_t flip(const vec_t& v) const noexcept {
 			const auto& nml = getNormal();
 			const value_t d = dot(v);
 			return {v + nml*-d*2};
 		}
 		//! 平面との交差点を算出
-		auto crosspoint(const vec_t& v0, const vec_t& v1) const {
+		auto crosspoint(const vec_t& v0, const vec_t& v1) const noexcept {
 			struct {
 				vec_t point;
 				bool cross;
