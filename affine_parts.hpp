b8edf1d0a7dc32f968446464300741f723737ca1
degarashi <slice013@gmail.com>
2016-09-11 13:48:35 +0900

    AffinePartsクラス追加

---
+++ affine_parts.hpp
@@ -0,0 +1,54 @@
+#pragma once
+#include "quaternion.hpp"
+
+namespace frea {
+	template <class T>
+	struct AffineParts : op::Ne<AffineParts<T>> {
+		using Vec = Vec_t<T,3,true>;
+		using Quat = QuatT<T,true>;
+		Vec		offset,
+				scale;
+		Quat	rotation;
+
+		template <class Ar>
+		void serialize(Ar& ar) {
+			ar(CEREAL_NVP(offset), CEREAL_NVP(scale), CEREAL_NVP(rotation));
+		}
+		bool operator == (const AffineParts& ap) const noexcept {
+			return offset == ap.offset &&
+				scale == ap.scale &&
+				rotation == ap.rotation;
+		}
+		//! アフィン成分分解
+		template <class M, ENABLE_IF((is_matrix<M>{} || is_wrapM<M>{}) && (M::dim_m==4 && M::dim_n>=3))>
+		static auto Decomp(const M& m) noexcept {
+			AffineParts<T> ap;
+			Mat_t<T, 3, 3, true> tm;
+			// オフセットは4行目をそのまま抽出
+			ap.offset = m.template getRow<3>().template convert<3>();
+			for(int i=0 ; i<3 ; i++) {
+				const auto r = m[i].template convert<3>();
+				ap.scale[i] = r.length();
+				tm[i] = r / ap.scale[i];
+			}
+			// 掌性チェックして、左手系でなければX軸を反転
+			const auto cv = tm.template getRow<1>().cross(tm.template getRow<2>());
+			if(tm.template getRow<0>().dot(cv) <= 0) {
+				tm.template getRow<0>() *= -1;
+				ap.scale.x *= -1;
+			}
+			ap.rotation = Quat::FromAxis(tm.template getRow<0>(), tm.template getRow<1>(), tm.template getRow<2>());
+			return ap;
+		}
+	};
+	template <class T>
+	inline std::ostream& operator << (std::ostream& os, const AffineParts<T>& ap) {
+		os << "AffineParts: offset=";
+		ap.offset.print(os);
+		os << ", scale=";
+		ap.scale.print(os);
+		os << ", rotation=";
+		ap.rotation.print(os);
+		return os;
+	}
+}
---
+++ tests/affineparts_test.cpp
@@ -0,0 +1,64 @@
+#include "test.hpp"
+#include "../affine_parts.hpp"
+
+namespace frea {
+	namespace test {
+		template <class T>
+		struct AffineParts : Random {
+			using value_t = T;
+			using AP = ::frea::AffineParts<value_t>;
+
+			auto makeAP(const Range<value_t>& rt, const Range<value_t>& rs) {
+				AP ap;
+				using Vec = Vec_t<value_t, 3, true>;
+				ap.offset = random::GenVec<Vec>(mt().template getUniformF<value_t>(rt));
+				ap.scale = random::GenVec<Vec>(mt().template getUniformF<value_t>(rs));
+				using Quat = QuatT<value_t, true>;
+				for(;;) {
+					ap.rotation = random::GenQuat<Quat>(mt().template getUniformF<value_t>());
+					try {
+						ap.rotation.getAxis();
+						break;
+					} catch(const NoValidAxis&) {}
+				}
+				return ap;
+			}
+			auto makeAP() {
+				const Range<value_t> r{-1e3, 1e3};
+				return makeAP(r,r);
+			}
+		};
+		using Types = ToTestTypes_t<types::Float_t>;
+		TYPED_TEST_CASE(AffineParts, Types);
+
+		TYPED_TEST(AffineParts, Test) {
+			// 行列をDecompAffineした結果を再度合成して同じかどうかチェック
+			const auto ap = this->makeAP({-1e3, 1e3}, {1e-2, 1e2});
+
+			USING(value_t);
+			using M33 = Mat_t<value_t, 3,3, true>;
+			using M44 = Mat_t<value_t, 4,4, true>;
+			const auto mR = M33::RotationAxis(ap.rotation.getAxis(), ap.rotation.angle()).template convertI<4,4,3>(1);
+			const auto mS = M44::Scaling(ap.scale.template convertI<4,3>(1));
+			const auto mT = M44::Translation(ap.offset.template convertI<4,3>(1));
+			const auto m = mS * mR * mT;
+
+			USING(AP);
+			const auto ap2 = AP::Decomp(m);
+			using V3 = Vec_t<value_t, 3, true>;
+
+			const auto mtf = this->mt().template getUniformF<value_t>();
+			auto dir0 = random::GenVecUnit<V3>(mtf),
+				dir1 = dir0;
+			dir0 *= ap.rotation;
+			dir1 *= ap2.rotation;
+			constexpr auto Th = ThresholdF<value_t>(0.5);
+			EXPECT_LT(AbsMax(V3(ap.offset - ap2.offset)), Th);
+			EXPECT_LT(AbsMax(V3(ap.scale - ap2.scale)), Th);
+			EXPECT_LT(AbsMax(V3(dir0 - dir1)), Th);
+		}
+		TYPED_TEST(AffineParts, Serialization) {
+			CheckSerialization(this->makeAP());
+		}
+	}
+}
