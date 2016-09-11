3f18110be716213ed2613eb0faae8a8f28fd328b
degarashi <slice013@gmail.com>
2016-09-11 13:48:47 +0900

    YawPitchDistクラス追加

---
+++ tests/yawpitchdist_test.cpp
@@ -0,0 +1,70 @@
+#include "test.hpp"
+#include "../yawpitchdist.hpp"
+
+namespace frea {
+	namespace test {
+		template <class T>
+		struct YawPitchDist : Random {
+			using value_t = T;
+		};
+		using Types = ToTestTypes_t<types::Float_t>;
+		TYPED_TEST_CASE(YawPitchDist, Types);
+
+		TYPED_TEST(YawPitchDist, FromPos_Random) {
+			USING(value_t);
+			const auto mtf = this->mt().template getUniformF<value_t>({-1e2, 1e2});
+			// YawPitchDistで計算した値を再度合成して結果を比較
+			using V3 = Vec_t<value_t, 3, true>;
+			V3 nzv;
+			do {
+				nzv = random::GenVec<V3>(mtf);
+			} while(nzv.length() < 1e-2);
+			const auto ypd = ::frea::YawPitchDist<value_t>::FromPos(nzv);
+			const auto res = ypd.toOffsetRot();
+			const auto nzv2 = -res.rot.getDir() * ypd.distance;
+
+			constexpr auto Th = ThresholdF<value_t>(0.8);
+			EXPECT_LT(AbsMax(V3(res.pos - nzv)), Th);
+			// rotation + distanceから復元した元座標ベクトル
+			EXPECT_LT(AbsMax(V3(nzv2 - nzv)), Th);
+		}
+
+		// XZ -> YawAngle
+		struct Yaw_Angle {
+			Vec3	dir;
+			DegF	angle;
+		};
+		const Yaw_Angle c_yawAngle[] = {
+			{{0,0,1}, DegF(0)},
+			{{1,0,1}, DegF(45)},
+			{{1,0,0}, DegF(90)},
+			{{1,0,-1}, DegF(135)},
+			{{0,0,-1}, DegF(180)},
+			{{-1,0,-1}, DegF(225)},
+			{{-1,0,0}, DegF(270)},
+			{{-1,0,1}, DegF(315)}
+		};
+		TYPED_TEST(YawPitchDist, FromPos_Fixed) {
+			USING(value_t);
+			using deg_t = Degree<value_t>;
+			const auto mtf = this->mt().template getUniformF<value_t>({-1,1});
+			// 予め答えが用意されたパターンと照らし合わせ
+			for(auto& ya : c_yawAngle) {
+				auto pos = ya.dir;
+				pos.y = mtf();	// Pitchはランダムな値
+				const auto ypd = ::frea::YawPitchDist<value_t>::FromPos(pos);
+				EXPECT_NEAR(deg_t(ya.angle).get(), deg_t(ypd.yaw).get(), ThresholdF<value_t>(0.8));
+			}
+		}
+		TYPED_TEST(YawPitchDist, Serialization) {
+			USING(value_t);
+			using rad_t = Radian<value_t>;
+			const auto mtf = this->mt().template getUniformF<value_t>();
+			::frea::YawPitchDist<value_t> ypd;
+			ypd.yaw = random::GenAngle<rad_t>(mtf);
+			ypd.pitch = random::GenHalfAngle<rad_t>(mtf);
+			ypd.distance = mtf({1e-2, 1e2});
+			CheckSerialization(ypd);
+		}
+	}
+}
---
+++ yawpitchdist.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "quaternion.hpp"
+
+namespace frea {
+	template <class T>
+	struct YawPitchDist : op::Ne<YawPitchDist<T>> {
+		using Rad = Radian<T>;
+		using Vec3 = Vec_t<T,3,true>;
+		using Quat = QuatT<T,true>;
+		Rad		yaw, pitch;
+		T		distance;
+		template <class Ar>
+		void serialize(Ar& ar) {
+			ar(CEREAL_NVP(yaw), CEREAL_NVP(pitch), CEREAL_NVP(distance));
+		}
+		bool operator == (const YawPitchDist& ypd) const noexcept {
+			return yaw==ypd.yaw &&
+					pitch==ypd.pitch &&
+					distance==ypd.distance;
+		}
+		//! 方向ベクトルをYaw,Pitch,Distanceに分解
+		template <class V, ENABLE_IF((is_vector<V>{} || is_wrap<V>{}))>
+		static auto FromPos(const V& pos) noexcept {
+			YawPitchDist<T> ypd;
+			// Distance
+			Vec3 v(pos);
+			ypd.distance = v.length();
+			v /= ypd.distance;
+
+			// Yaw
+			Vec3 xzvec(v.x, 0, v.z);
+			if(xzvec.len_sq() < ThresholdF<T>(0.4))
+				ypd.yaw.set(0);
+			else {
+				xzvec.normalize();
+				T ac = std::acos(Saturate<T>(xzvec.z, 1));
+				if(xzvec.x < 0)
+					ac = 2*Pi<T> - ac;
+				ypd.yaw.set(ac);
+			}
+
+			// Pitch
+			constexpr T h = AngleInfo<Radian_t>::one_rotation<T> / 2;
+			ypd.pitch.set(Saturate(std::asin(Saturate<T>(v.y, 1)), -h, h));
+			return ypd;
+		}
+		//! YawPitchDistの位置から座標原点を見る姿勢
+		auto toOffsetRot() const {
+			struct {
+				Vec3	pos;
+				Quat	rot;
+			} ret;
+			// Z軸をYaw/Pitch/Roll傾けた方向に対してDist距離進んだ場所がカメラの位置
+			// カメラの方向は変換済みZ軸と逆
+			const Quat q = Quat::RotationYPR(yaw, pitch, RadF(0));
+			const Vec3 z = q.getDir();
+			ret.pos = z*distance;
+			const Vec3 vd = -(z*distance).normalization();
+			ret.rot = Quat::LookAt(vd, Vec3(0,1,0));
+			return ret;
+		}
+	};
+	template <class T>
+	inline std::ostream& operator << (std::ostream& os, const YawPitchDist<T>& ypd) {
+		return os << "YPD: yaw=" << ypd.yaw << " pitch=" << ypd.pitch << " dist=" << ypd.distance;
+	}
+}
