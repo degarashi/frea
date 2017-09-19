d6ccb280be422c27da7d470302ce2a4fdde0cf41
degarashi <slice013@gmail.com>
2017-09-19 17:30:39 +0900

    いくつかの定数値を追加

--- constant.hpp
+++ constant.hpp
@@ -19,4 +19,15 @@ namespace frea {
 	}
 	template <class T>
 	constexpr T Pi = T(3.1415926535);	// std::atan(1.0f)*4
+	constexpr float PI = Pi<float>,
+					SIN0 = 0,
+					SIN30 = 1.f/2,
+					SIN45 = 1.f/1.41421356f,
+					SIN60 = 1.7320508f/2,
+					SIN90 = 1.f,
+					COS0 =  SIN90,
+					COS30 = SIN60,
+					COS45 = SIN45,
+					COS60 = SIN30,
+					COS90 = SIN0;
 }
