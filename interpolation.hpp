92243fca0e2e7fc7f5b20199e961af363f8bd9c0
degarashi <slice013@gmail.com>
2018-04-14 17:47:15 +0900

    Lerp(): Quaternionの特殊化

--- interpolation.hpp
+++ interpolation.hpp
@@ -1,8 +1,14 @@
 #pragma once
+#include "detect_type.hpp"
+#include "lubee/meta/enable_if.hpp"
 
 namespace frea {
-	template <class T>
-	T Lerp(const T& t0, const T& t1, const T& t) noexcept {
-		return t0 + (t1-t0)*t;
+	template <class V, class T, ENABLE_IF(!is_quaternion<V>{})>
+	auto Lerp(const V& v0, const V& v1, const T& t) noexcept {
+		return v0 + (v1-v0)*t;
+	}
+	template <class V, class T, ENABLE_IF(is_quaternion<V>{})>
+	auto Lerp(const V& v0, const V& v1, const T& t) noexcept {
+		return v0.slerp(v1, t);
 	}
 }
